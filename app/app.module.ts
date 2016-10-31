9abbc1df3efec94f73277a39dccd65acd9876e08
rerost <gmailii@yahoo.co.jp>
2016-11-01 07:42:58 +0900

    Finished chapter3

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,17 @@ import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
+import { HeroDetailComponent } from './hero-detail.component';
+
 @NgModule({
-  imports:      [
+  imports: [
     BrowserModule,
     FormsModule
   ],
   declarations: [
     AppComponent,
+    HeroDetailComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
